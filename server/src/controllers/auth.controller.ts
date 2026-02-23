c0788bc22a8640fc59f23ae888fc1b4db7a56742
iamdanwi <danwichoudhary@gmail.com>
2026-02-23 16:39:04 +0530

    updated auth time

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,18 +1,18 @@
-import type {Response, Request} from "express";
-import {usersTable} from "../schema/schema.js";
+import type { Response, Request } from "express";
+import { usersTable } from "../schema/schema.js";
 import db from "../config/db.js";
-import {eq} from "drizzle-orm";
+import { eq } from "drizzle-orm";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
-export const registerUser = async(req: Request, res: Response) => {
+export const registerUser = async (req: Request, res: Response) => {
     try {
-        const {name, email, password} = req.body;
+        const { name, email, password } = req.body;
 
         const existingUser = await db.select().from(usersTable).where(eq(usersTable.email, email));
 
-        if(existingUser.length > 0) {
-            return res.status(400).json({message: "User already exists"});
+        if (existingUser.length > 0) {
+            return res.status(400).json({ message: "User already exists" });
         }
 
         const hashedPassword = await bcrypt.hash(password, 8);
@@ -23,35 +23,35 @@ export const registerUser = async(req: Request, res: Response) => {
             password: hashedPassword
         });
 
-        return res.status(201).json({message: "Account created, please login"});
+        return res.status(201).json({ message: "Account created, please login" });
 
-    }catch (error : string | any | undefined) {
-        res.status(500).send({error: error.message});
+    } catch (error: string | any | undefined) {
+        res.status(500).send({ error: error.message });
     }
 }
 
-export const loginUser = async(req: Request, res: Response) => {
+export const loginUser = async (req: Request, res: Response) => {
 
     try {
-        const {email, password} = req.body;
+        const { email, password } = req.body;
 
         const existingUser = await db.select().from(usersTable).where(eq(usersTable.email, email));
 
-        if(existingUser.length === 0){
-            return res.status(400).json({message: "User does not exists"});
+        if (existingUser.length === 0) {
+            return res.status(400).json({ message: "User does not exists" });
         }
 
         const user = existingUser[0]!;
 
         const checkPassword = await bcrypt.compare(password, user.password);
-        if(!checkPassword){
-            return res.status(400).json({message: "Either email or password is incorrect"});
+        if (!checkPassword) {
+            return res.status(400).json({ message: "Either email or password is incorrect" });
         }
 
-        const token = jwt.sign({userId: user.id}, process.env.JWT_SECRET as string, {expiresIn: '1d'});
+        const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET as string, { expiresIn: '7d' });
 
-        return res.status(200).json({token: token, message: "Successfully logged in"});
-    }catch (error : string | any | undefined) {
-        res.status(500).send({error: error.message});
+        return res.status(200).json({ token: token, message: "Successfully logged in" });
+    } catch (error: string | any | undefined) {
+        res.status(500).send({ error: error.message });
     }
 }
